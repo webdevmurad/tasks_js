8b040c895e1e8df3b8539655a11dce5b60673811
Murad <murad-arslangiraev-1994@mail.ru>
2019-09-21 00:48:15 +0300

    чуть правки

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// cycle Task 
+
+/*
+for (let i = 1; i <= 5; i++){
+  console.log(i);
+  for (let k = 1; k <= i; k++){
+    document.write(i);
+  }
+
+  document.write('<br>');
+}
+
+   what shout come out in document
+   1
+   22
+   333
+   4444
+   55555
+*/
+
+
+// cycle Task
+
+
+/*
 for (let i = 1; i <= 5; i++){
   console.log(i);
   for (let k = 1; k <= i; k++){
@@ -6,7 +31,7 @@ for (let i = 1; i <= 5; i++){
 
   document.write('<br>');
 }
-/*
+
    what shout come out in document
 
    1
