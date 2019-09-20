0174a41efb82580f694e72d1592f586bf2b3059f
Murad <murad-arslangiraev-1994@mail.ru>
2019-09-21 00:43:08 +0300

    новая решенная задачка

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 for (let i = 1; i <= 5; i++){
   console.log(i);
   for (let k = 1; k <= i; k++){
-    document.write(i);
+    document.write(k);
   }
 
   document.write('<br>');
@@ -10,8 +10,8 @@ for (let i = 1; i <= 5; i++){
    what shout come out in document
 
    1
-   22
-   333
-   4444
-   55555
+   12
+   123
+   1234
+   12345
 */
